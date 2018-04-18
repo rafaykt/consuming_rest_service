50be1d8398d3d3858f5e9447448ef4a4f808491a
webdev2 <webdev2@SENSATTA.COM>
2018-04-18 18:01:44 -0300

    Aplicação para consumo de json

---
+++ ConsoleApp1/ConsoleApp1/ObjectPackage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    [DataContract]
+    public class ObjectPackage
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public bool isComplete { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*URL do site*/
+            var url = "http://10.0.0.13:8020/api/todo";
+
+            /*Cliente sincrono que vai consumir a aplicação*/
+            var syncClient = new WebClient();
+            var content = syncClient.DownloadString(url);
+
+
+            /*Criaçao do parser do json*/
+
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(ObjectPackage));
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(content)))
+            {
+                var obj = (ObjectPackage)serializer.ReadObject(ms);
+            }
+           
+            
+            /// <sumary>
+            /// pega os dados de uma app json
+            /// </sumary>
+            /// <param name="id"
+
+        }
+    }
+}
